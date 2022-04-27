7afbeba7ddc23c4ad674e51ed2575842cfca4e17
ViníciusYoda <rm92969@fiap.com.br>
2022-04-27 12:09:41 -0300

    up

--- python/alarme.py
+++ python/alarme.py
@@ -1,3 +1,9 @@
+from time import sleep
+
+ha = int(input('Hora: '))
+ma = int(input('Minutos: '))
+sa = int(input('Segundos'))
+
 h = 0
 while h < 24:
     m = 0 
@@ -5,17 +11,18 @@ while h < 24:
         s = 0
         while s < 60:
             print(f'{h:02}:{m:02}:{s:02}')
-            if h==23 and m==55 and s==58:
+            if h==ha and m==ma and s==sa:
+                sleep(1)
                 print('Alarme')
                 break
             s+=1
-        if h==23 and m==55:
+        if h==ha and m==ma:
             break
         m+=1
-    if h==23:
+    if h==ha:
         break
     h+=1
     
     print("Alarme")
     
-    ☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !"#"#$%&'())*)+♦♦-./0012348;<=>6♥?@ABEKPUWQXY[][]\]\_``abdc☺efinsvwxyxyz{}|}~⌂Çüéâäàåççêëèï◙ìÄÅÉæÆ
+   # ☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !"#"#$%&'())*)+♦♦-./0012348;<=>6♥?@ABEKPUWQXY[][]\]\_``abdc☺efinsvwxyxyz{}|}~⌂Çüéâäàåççêëèï◙ìÄÅÉæÆ
