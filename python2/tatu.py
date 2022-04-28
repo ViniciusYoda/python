02ef753d48ad9064f789557208a540c7b10dad77
Logon Aluno <90533635+ViniciusYoda@users.noreply.github.com>
2022-04-28 10:15:23 -0300

    TATU

--- python2/tatu.py
+++ python2/tatu.py
@@ -7,7 +7,7 @@ while valor <= credito:
     credito -= valor
     print(f'Crédito: {credito:.2f}')
     valor = float(input('Valor: '))
-
+""
 if valor > credito:
     print('Valor acima do seu crédito')
 print(f'Total da compra:  {total:.2f}')
