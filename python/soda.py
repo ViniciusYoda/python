8f365cd2244ac6e80745289791d29a0fc520c5e7
ViníciusYoda <rm92969@fiap.com.br>
2022-05-12 16:56:56 -0300

    soja está joia

--- python/soda.py
+++ python/soda.py
@@ -1,5 +1,16 @@
 nome = "Vinícius Yoda"
 empresa = 'FIAP'
 lab = 505
+valor = 1135.00
 print(nome + " estuda na faculdade " + empresa )
 print("Ele também esuda na lab: ", lab)
+print("A mensalidade da: " + empresa + " é de: " + str(valor))
+
+nome=input('Digite um aluno: ')
+empresa=input("Digite a faculdade que ele estuda: ")
+lab=int(input("Qual lab oestuda?: "))
+valor=float(input('Quanto o ' + nome + 'paga de mensalidade?: '))
+print(nome + ' estuda na faculdade ' + empresa)
+print(nome + ' também estuda no lab ' + str(lab))
+print('A mensalidade que ' + nome + ' paga é de: ' + str(valor))
+
