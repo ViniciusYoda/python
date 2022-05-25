93b99259e26bd2baaba08e1c9aae1bf30d25c97b
ViníciusYoda <rm92969@fiap.com.br>
2022-05-25 11:54:33 -0300

    Sistema operacional

---
+++ python/carro.py
@@ -0,0 +1,48 @@
+print("Qual o melhor Sistema Operacional para uso em servidores?")
+print('''1 - Windows Server 2- Unix 3-Linux 4- Netware, 5- Mac OS, 6- Outro''')
+
+opcoes = ['Windows Server', 'Unix', 'Linux', 'Netware', 'Mac OS',  'Outro']
+sistemas = [0] * 6
+
+while True:
+    #VAlidar a entrada do usuário
+    while True:
+        opcao = int(input('Digite a opção: '))
+        if opcao < 0 or opcao > 6:
+            print('Opçao inválida')
+        else:
+            break
+    if opcao == 0:
+        break
+    elif opcao == 1:
+        sistemas[0] += 1
+    elif opcao == 2:
+        sistemas[1] += 1
+    elif opcao == 3:
+        sistemas[2] += 1
+    elif opcao == 4:
+        sistemas[3] += 1
+    elif opcao == 5:
+        sistemas[4] += 1
+    elif opcao == 6:
+        sistemas[5] += 1
+    
+#sistemas[opcao-1] += 1
+
+print('Sistema Operacional     Votos %')
+print('-----------------------------------') 
+cont = 0
+melhor = 0
+melhorSis = ''
+perc = 0
+for s in sistemas:
+    print(f'{opcoes[cont]} {s} {(s*100)/sum(sistemas)}')
+    if s > melhor:
+        melhor = s
+        melhorSis = opcoes[cont]
+        perc = (s * 100) / sum(sistemas)
+    cont += 1
+
+print('----------------------------')
+print(f'Total: {sum(sistemas)}')
+print('O sistemas operacional mais votado foi {melhorSis} com {melhor} votos  correspone{perc}')
