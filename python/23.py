7fc87d3b7e2126316d59c9630defa14c6c9c163f
ViníciusYoda <rm92969@fiap.com.br>
2022-06-01 17:44:24 -0300

    23

---
+++ python/23.py
@@ -0,0 +1,6 @@
+#Crie um programa que exiba o alfabeto minúsculo e maiúsculo.
+for letra in 'qwertyuiopasdfghjklzxcvbnm':
+    print(letra)
+print()
+for letra in 'MNBVCXZLKJHGFDSAPOIUYTREWQ':
+    print(letra)
