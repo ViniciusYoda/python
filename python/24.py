7646dee614968f625176369d4edf33844458cb50
ViníciusYoda <rm92969@fiap.com.br>
2022-06-01 17:48:26 -0300

    24

---
+++ python/24.py
@@ -0,0 +1,6 @@
+#Crie um programa que exiba, em ordem crescente, os pares de 10 até 100 e, em ordem descrecente, os ímpares de 100 até 10.
+for par in range(10, 101, 2):
+    print(par, end=' ')
+print()
+for impar in range(99, 10, - 2):
+    print(impar, end= ' ')
