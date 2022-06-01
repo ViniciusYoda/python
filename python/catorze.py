08ca552588d76dacc6a92979f604bfb7aa08fbeb
ViníciusYoda <rm92969@fiap.com.br>
2022-06-01 15:45:20 -0300

    catorze

---
+++ python/catorze.py
@@ -0,0 +1,7 @@
+executa = input('Executar o bloco ')
+contador = 0
+while executa == "sim":
+    contador  +=1
+    executa = input('Executar o bloco ')
+
+print(f'O bloco foi executador {contador} vezes ')
