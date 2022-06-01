a0f8a584f2a6ec62777d2ce5ace58f641ed5fa4e
ViníciusYoda <rm92969@fiap.com.br>
2022-06-01 11:17:30 -0300

    cinco

---
+++ python/cinco.py
@@ -0,0 +1,7 @@
+from tracemalloc import DomainFilter
+
+
+dolar = float(input("Qual o valor do dolar? "))
+quantidade = int(input("Qual a quantidade que deseja comprar: "))
+valor = quantidade * dolar
+print(f"O valor do dolar é de: {dolar:.2f}, com isso você gastará {valor:.2f} reais")
