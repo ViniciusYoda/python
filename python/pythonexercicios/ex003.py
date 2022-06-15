a1ec0acf96bda3b454cd460923ac43438c15b2e7
ViníciusYoda <rm92969@fiap.com.br>
2022-06-15 15:47:58 -0300

    ex004

--- python/pythonexercicios/ex003.py
+++ python/pythonexercicios/ex003.py
@@ -1,4 +1,4 @@
 n1 = int(input("Digite um valor: "))
 n2 = int(input("Digite o segundo valor: "))
 s = n1 + n2
-print('A soma entre {} e {} é de: {}.format'{n1} {n2} {s})
+print('A soma entre {} e {} é de: {}.'.format(n1, n2, s))
---
+++ python/pythonexercicios/êx004.py
@@ -0,0 +1,4 @@
+a = input('Digite algo: ')
+print("O tipo primitivo dessse valor é ", type(a))
+print("Só tem espaço? ", a.isspace())
+print("")
