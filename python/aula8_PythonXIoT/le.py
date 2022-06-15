ec68bcafce2f706b0430f6e2f0bf15f316eba6c1
ViníciusYoda <rm92969@fiap.com.br>
2022-06-15 20:41:54 -0300

    del

---
+++ python/aula8_PythonXIoT/le.py
@@ -0,0 +1,17 @@
+usuarios={}
+
+usuarios={
+
+"Ana":["Ana Silva","17/06/1975","RH"],
+
+"Jose":["Jose Flores","03/06/1976","Financeiro"],
+
+"Ana":["Ana Silva","03/06/1976","Cozinheira"]
+
+}
+
+usuarios["Ana"]=["Ana Souza", "03/06/1976", "Telefonista"]
+
+usuarios["Florinda"]=["Florinda Flores", "26/11/2016", "Recepcionista"]
+
+print(usuarios)
