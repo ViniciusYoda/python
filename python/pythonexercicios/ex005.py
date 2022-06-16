7a9e59ca4ab29f17190b065b1af4e27779f83ae1
ViníciusYoda <rm92969@fiap.com.br>
2022-06-16 15:40:04 -0300

    antecessor, sucessor, dobro, triplo, raiz quadrada

---
+++ python/pythonexercicios/ex005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número: '))
+suc = n + 1
+ant = n -1
+print('Analisando o numero {}, o seu antecessor é {} e o seu sucessor é {}'.format(n, ant, suc))
---
+++ python/pythonexercicios/ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+dobro = n * 2
+triplo = n * 3
+rq = n ** (1/2)
+print('O dobro de {} é de: {}.'.format(n, dobro))
+print('O triplo de {} é de: {}.'.format(n, triplo))
+print('A raiz quadrade de {} é de: {}'.format(n, rq))
