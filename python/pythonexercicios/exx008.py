68d62f427c4c64a00882cc4136b1af59ad47d03a
ViníciusYoda <rm92969@fiap.com.br>
2022-06-17 09:29:17 -0300

    metros, cm, mm

---
+++ python/pythonexercicios/exx008.py
@@ -0,0 +1,4 @@
+metros = float(input('Digite o valor em metros: '))
+cm = metros * 100
+mm = metros * 1000
+print('A medida de {:.0f}m em cm é de {:.0f}cm e em mm é de {:.0f}mm.'.format(metros, cm, mm))
