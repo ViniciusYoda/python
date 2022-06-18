cc350ec2862b4aebc4a12e41d02200c7f6d2a691
ViníciusYoda <rm92969@fiap.com.br>
2022-06-18 16:55:51 -0300

    calculando desconto

---
+++ python/pythonexercicios/ex012.py
@@ -0,0 +1,3 @@
+preco = float(input('Qual é o preço do produto? R$'))
+desconto = preco - (preco*(5/100))
+print('O produto que custava R${}, na promoção com desconto de 5% vai custar R${:.2f}.'.format(preco, desconto))
