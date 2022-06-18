d411f463f90ad37cf5e5e508933bbcbc382c4afe
ViníciusYoda <rm92969@fiap.com.br>
2022-06-18 16:43:55 -0300

    calculando litros de tinta

---
+++ python/pythonexercicios/ex011.py
@@ -0,0 +1,9 @@
+from traceback import print_tb
+
+
+largura = float(input('Largura da parede: '))
+altura = float(input('Altura da parede: '))
+area = largura * altura
+tinta = area / 2
+print('Sua parede tem a dimensão de {}x{} e sua área é de {:.2f}m².'.format(largura, altura, area))
+print('Para pintar a parde, você precisará de {:.2f}l de tinta'.format(tinta))
