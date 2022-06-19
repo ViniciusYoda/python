4c325aa488b1f9d1f4df571b1e5419dafadaea48
ViníciusYoda <rm92969@fiap.com.br>
2022-06-19 18:10:18 -0300

    modulo

---
+++ python/pythonteste/aula08a.py
@@ -0,0 +1,9 @@
+import math
+num =  int(input('Digite um número: '))
+raiz = math.sqrt(num)
+n = 2
+print(math.ceil(raiz))
+print(math.pow(raiz, n))
+print(math.log(raiz))
+print(math.exp(raiz))
+print(math.fabs(raiz))
