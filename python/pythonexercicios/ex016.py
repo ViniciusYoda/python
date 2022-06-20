201a26950484f3c3c0364fea1720c6fd1ce4edee
ViníciusYoda <rm92969@fiap.com.br>
2022-06-20 13:49:15 -0300

    Convertendo float para python

---
+++ python/pythonexercicios/ex016.py
@@ -0,0 +1,6 @@
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
+
+n = float(input('Digite um número'))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(n, int(n)))
