c4b86f387da71d681e68bbe9e336f82263888d56
ViníciusYoda <rm92969@fiap.com.br>
2022-06-20 13:57:39 -0300

    calculando hipotenusa

---
+++ python/pythonexercicios/ex017.py
@@ -0,0 +1,7 @@
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+#hi = (co ** + ca ** 2) ** (1/2)
+#print('A hipotenusa vai medir {:.2f}'.format(hi))
