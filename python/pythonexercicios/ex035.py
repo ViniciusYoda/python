1c8dea49948bf4ae317c807fb3f0f5e54d25143b
ViníciusYoda <rm92969@fiap.com.br>
2022-07-02 11:28:42 -0300

    Vendo se forma um triângulo

---
+++ python/pythonexercicios/ex035.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+
+
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima podem formar um triângulo')
+else:
+    print('Os segmentos acima não pode formar um triângulo')
