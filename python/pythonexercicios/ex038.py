b3e45a0c076b8ef8b1ca04d4e295321f5a903ba6
ViníciusYoda <rm92969@fiap.com.br>
2022-07-04 17:41:42 -0300

    Comparando números

---
+++ python/pythonexercicios/ex038.py
@@ -0,0 +1,8 @@
+primeiro = int(input('Primeiro número: '))
+segundo = int(input('Segundo número: '))
+if primeiro > segundo:
+    print("O primeiro número é maior")
+elif primeiro == segundo:
+    print("Os dois valores são iguais")
+else:
+    print("O segundo número é maior")
