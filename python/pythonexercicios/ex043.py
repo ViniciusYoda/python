1cd0a266795bd86efa43d3e4c80372f2a1381126
ViníciusYoda <rm92969@fiap.com.br>
2022-07-06 11:31:19 -0300

    IMC

---
+++ python/pythonexercicios/ex043.py
@@ -0,0 +1,14 @@
+peso = float(input('Qual é seu peso? (km) '))
+altura = float(input('Qual é a sua altura (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc <= 18.5:
+    print('Abaixo do Peso')
+elif imc <= 25:
+    print('Peso ideal')
+elif imc <= 30:
+    print('Sobrepeso')
+elif imc <= 40:
+    print('Obesidade')
+else:
+    print('Obesidade mórbida')
