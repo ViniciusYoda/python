dd89896a6560b982d851f0646d57ad1d61cf5e4c
ViníciusYoda <rm92969@fiap.com.br>
2022-07-14 15:56:17 -0300

    PA v3

---
+++ python/pythonexercicios/ex062.py
@@ -0,0 +1,17 @@
+print('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} → '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('PA finalizada com {} termos mostrados'.format(total))
