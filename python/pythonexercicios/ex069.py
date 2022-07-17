60b1b7b612af6f9b57aa0b772c9c77ffd9a6e8e4
ViníciusYoda <rm92969@fiap.com.br>
2022-07-17 13:42:45 -0300

    Analise de dados do grupo

---
+++ python/pythonexercicios/ex069.py
@@ -0,0 +1,20 @@
+tot18 = totH = totF20 = 0
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    if idade >= 18:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if totF20 == 'F' and idade < 20:
+        totF20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {tot18}')
+print(f'Total de homens cadastrados: {totH}')
+print(f'Total de mulheres com menos de 20 anos {totF20}')
