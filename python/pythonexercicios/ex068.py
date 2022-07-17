03b0fc0c9f1c17d417fe2494f8fdf7e6a1d398e1
ViníciusYoda <rm92969@fiap.com.br>
2022-07-17 13:30:36 -0300

    Par ou impar

---
+++ python/pythonexercicios/ex068.py
@@ -0,0 +1,26 @@
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Diga um valor: '))
+    computador = randint(0, 10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Impar? [p/i] ')).strip().upper()[0]
+    print(f"Você jogou {jogador} e o computador {computador}. Total de {total} ", end="")
+    print('Deu par' if total % 2 == 0 else 'Deu impar')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você venceu')
+            v += 1
+        else:
+            print('Você perdeu')
+    print('Vamos jogar novamente...')
+print(f'Game Over. Você venceu {v} vezes')
