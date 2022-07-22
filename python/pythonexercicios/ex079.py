a34c9b4a1484659f4a8350ac37ea66d51b798b04
ViníciusYoda <rm92969@fiap.com.br>
2022-07-22 13:34:39 -0300

    Valores unicos em uma lista

---
+++ python/pythonexercicios/ex079.py
@@ -0,0 +1,12 @@
+numeros = list()
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valora adicionado')
+    else:
+        print('Valor duplicado, não vou adicionar')
+    r = str(input('Quer continuar? [S/N] '))
+    if r in 'Nn':
+        break
+print(f'Você digitous os valores: {numeros}')
