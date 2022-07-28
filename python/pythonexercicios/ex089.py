3c86b08cdee2f80dbff4a03b06e99e4880b6200f
ViníciusYoda <rm92969@fiap.com.br>
2022-07-28 14:11:04 -0300

    Grupo de notas na lista

---
+++ python/pythonexercicios/ex089.py
@@ -0,0 +1,20 @@
+ficha = list()
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar?: '))
+    if resp in 'Nn':
+        break
+print(f'{"No.":<4}{"Nome":<10}{"Média":>8}')
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    opc = int(input('Mostrar notas de qual aluno? [999] para sair: '))
+    if opc == 999:
+        print('FInalizando')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
