bf131f3fc6198adf26fbd525d739731fce6c65b6
ViníciusYoda <rm92969@fiap.com.br>
2022-08-03 16:07:52 -0300

    Unido dicionario com lista

---
+++ python/pythonexercicios/ex094.py
@@ -0,0 +1,38 @@
+galera = list()
+pessoa = dict()
+soma = media = 0
+while True: 
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('Erro. Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N]')).upper()[0]
+        if resp in 'SN':
+            break
+        print('Erro! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+
+print(f'Temos {len(galera)} pessoas cadastradas')
+media = soma / len(galera)
+print(f'A média de idade é de {media:5.2f} anos')
+print('As mulheres cadastradas foram: ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]} ', end='')
+print()
+print('Lista da pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('     ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}: ', end='')
+        print()
+print('Fim do programa')
