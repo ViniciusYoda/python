21ed0a3d69c1b779bbfd2d901c8443741f601c39
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-04 09:43:34 -0300

    def calculadora

--- python/facul/funcao.py
+++ python/facul/funcao.py
@@ -1,5 +1,70 @@
-def soma(n1, n2):
+print('Calculadora simples')
+
+def entrada_de_dados():
+    print('Entrada de dados ')
+    n = int(input('Digite um número: '))
+    return n
+
+def menu():
+    print('*- Menu -*')
+    print('='*10)
+    print('1- Adição ')
+    print('2- Subtração ')
+    print('3 - Multiplicação ')
+    print('4 - Divisão')
+    op = int(input('Opção: '))
+    return op
+
+
+def add(n1, n2):
+    print('Adição')
     s = n1 + n2
     return s
 
-print(soma(6, 7))
+
+
+def sub(n1, n2):
+    print('Subtração')
+    s = n1 - n2
+    return s
+
+
+def mul(n1, n2):
+    print('Multiplicação')
+    s = n1 * n2
+    return s
+
+
+def div(n1, n2):
+    print('Divisão')
+    s = n1 / n2
+    return s
+
+def imprimir(result):
+    print('Imprimir o resultado')
+    print(f'Resultado: {result}')
+
+def controlador(n1, n2, op):
+    print('Controlador')
+    if op == 1:
+        r = add(n1, n2)
+    elif op == 2:
+        r = sub(n1, n2)
+    elif op == 3:
+        r = mul(n1, n2)
+    elif op == 4:
+        r = div(n1, n2)
+    else:
+        r = 'Opção inválida'
+    return r
+           
+print('*- Principal -*')
+
+op = menu()
+n1 = entrada_de_dados()
+n2 = entrada_de_dados()
+result = controlador(n1, n2, op)
+imprimir(result)
+
+
+
