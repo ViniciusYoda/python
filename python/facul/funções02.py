11b0faf1c6dacd78c0837035c625a6b60db825f5
ViníciusYoda <rm92969@fiap.com.br>
2022-08-10 13:34:45 -0300

    funções 02

---
+++ python/facul/funções02.py
@@ -0,0 +1,16 @@
+def entrada_de_dados():
+    n = int(input('Digite um número: '))
+    return n
+
+def dobro(n):
+    n * 2
+    return n
+
+def imprimir(result):
+    print(f'O dobro de {n} é {result}')
+
+# principal
+
+num = entrada_de_dados()
+dobro = dobro()
+resultado = result(n, result)
