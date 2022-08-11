518a97bb28d4a743ea910e1d5baa8b3fe9ff8064
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-11 10:13:37 -0300

    funções

--- python/facul/funções08.py
+++ python/facul/funções08.py
@@ -1,6 +1,23 @@
+import math
+
 def entrada_de_dados():
     n = int(input('Digite um número: '))
     return n
 
-def calcularEquacao2():
-    
+def calcularEquacao2(a, b, c):
+    delta = b ** 2 - 4 * a * c
+    return delta
+
+def raiz(a, b, c, delta):
+    x = - (-b) + math.sqrt(delta) / 2 * a
+    y = - (-b) - math.sqrt(delta) / 2 * 4
+
+def imprimir(result):
+    print(f'A raiz maior do delta é {result}')
+
+a = entrada_de_dados()
+b = entrada_de_dados()
+c = entrada_de_dados()
+equacao = calcularEquacao2(a,b,c)
+raiz = raiz(equacao)
+imprimir(raiz)
