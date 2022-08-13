707e899fc4abc47b21862e32ad669242edc8e1ad
ViníciusYoda <rm92969@fiap.com.br>
2022-08-13 15:52:22 -0300

    funções

--- python/pythonteste/aula21.py
+++ python/pythonteste/aula21.py
@@ -7,3 +7,23 @@ def somar(a = 0, b = 0, c = 0):
 
 somar(3,2,5)
 somar(8, 4)
+
+def fatorial(num = 1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+n = int(input('Digite um número: '))
+print(fatorial(n))
+
+def par(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+num = 4
+if par(num):
+    print('è par')
+
