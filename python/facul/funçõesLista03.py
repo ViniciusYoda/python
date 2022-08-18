fed98e3c872689109904caf8d53631ec4288cf8f
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-18 07:48:58 -0300

    Funções com lista

--- python/facul/funçõesLista03.py
+++ python/facul/funçõesLista03.py
@@ -15,10 +15,51 @@ def imprimir_lista(lista):
     for i in lista:
         print(f'Elemento: {i}')
 
+def imprimir_pares(lista):
+    for i in lista:
+        if i % 2 == 0:
+            print(f'Números pares: {i}')
+
+def imprimir_impares(lista):
+    for i in lista:
+        if i % 2 != 0:
+            print(f'Números ímpares: {i}')
+
+def adicionar_pares(lista):
+    par = int(input('Digite um número par: '))
+    if par % 2 == 0:
+        lista.append(par)
+    else:
+        print('Número não adicionado')
+    return par
+
+def adicionar_impares(lista):
+    impar = int(input('Digite um número ímpar: '))
+    if impar % 2 != 0:
+        lista.append(impar)
+    else:
+        print('Número não adicionado')
+    return impar
+
+def buscar_elemento(lista):
+    busca = int(input('Por qual elemento você está procurando? '))
+    if busca in lista:
+        print(f'O núemro {busca} está na lista')
+    else:
+        print(f'O número {busca} não está na lista')
+    return busca
+
+
 def principal():
     t = tamanho_lista()
     lista = criar_lista(t)
     imprimir_lista(lista)
+    imprimir_pares(lista)
+    imprimir_impares(lista)
+    adicionar_pares(lista)
+    adicionar_impares(lista)
+    imprimir_lista(lista)
+    buscar_elemento(lista)
 
 # programa principal
 principal()
