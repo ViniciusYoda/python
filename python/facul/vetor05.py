30c590809164746b9b768ac74268bc852abdfa43
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-25 08:49:18 -0300

    vetor e matriz

---
+++ python/facul/vetor05.py
@@ -0,0 +1,22 @@
+from math import factorial
+
+
+def numeros():
+    lista = [5, 4, 3, 2, 1]
+    return lista
+
+def produtos(lista):
+    fat = 1
+    for i in lista:
+        fat *= i
+    return fat
+
+def imprimir(resultado):
+    print(resultado)
+
+def principal():
+    numero = numeros()
+    produto = produtos(numero)
+    imprimir(produto)
+
+principal()
