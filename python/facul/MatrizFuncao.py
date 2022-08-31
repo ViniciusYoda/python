ef78776f11dccc445d486e1fa13f3cd5dafeaa64
ViníciusYoda <rm92969@fiap.com.br>
2022-08-31 10:39:39 -0300

    matriz

---
+++ python/facul/MatrizFuncao.py
@@ -0,0 +1,10 @@
+''''''
+def criar_matriz():
+    matriz = []
+    return matriz
+def somar(matriz, linha, coluna, valor):
+    for l in matriz[]:
+        
+matriz = criar_matriz(3, 3, 8)
+somar(matriz, linha=3, coluna=3, valor=8, v=0)
+print(somar)
