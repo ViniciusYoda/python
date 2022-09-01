e006ee607b6f6f47fb37c862b519c0a71f0729ab
Vinícius Yoda <vyoda4604@gmail.com>
2022-09-01 07:33:51 -0300

    organizando as pastas

--- python/facul/funcao/funções11.py
+++ python/facul/funcao/funções11.py
@@ -1,20 +1,22 @@
-def media_final():
-    media = float(input('Qual é a sua média final? '))
-    return media
+def obter_idade():
+    idade = int(input('Digite a sua idade: '))
+    return idade
 
-def nota(media):
-    if media <= 4.9:
-        msg = 'D'
-    elif media <= 6.9:
-        msg = 'C'
-    elif media <= 8.9:
-        msg = 'B'
-    elif media <= 10:
-        msg = 'A'
+def categoria(idade):
+    if idade > 4 and idade < 8:
+        msg = 'Infatil A'
+    elif idade > 7 and idade < 11:
+        msg = 'Infatil B'
+    elif idade > 10 and idade < 14:
+        msg = 'Juvenil A'
+    elif idade > 14 and idade < 18:
+        msg = 'Juvenil B'
+    else:
+        msg = 'Adulto'
     return msg
 
 def principal():
-    media = media_final()
-    print(nota(media))
+    idade = obter_idade()
+    print(categoria(idade)) 
 
 principal()
