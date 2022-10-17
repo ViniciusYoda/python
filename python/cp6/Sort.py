80f2b85961bd62166e0f3717a86714fff86c115a
Vinícius Yoda <vyoda4604@gmail.com>
2022-10-17 19:53:24 -0300

    sort

--- python/cp6/Sort.py
+++ python/cp6/Sort.py
@@ -1,4 +1,50 @@
 import time
+import random
+
+def bubbleSort(lista):
+    '''
+    Função que ordena a lista um por um
+    '''
+    for c in range(len(lista), 0, -1):
+        ordenado = False
+        for i in range(0, c - 1):
+            if lista[i] > lista[i + 1]:
+                lista[i], lista[i + 1] = lista[i + 1], lista[i]
+                ordenado = True
+        if not ordenado:
+            break
+
+def dezMil():
+    lista = []
+    for i in range(random.randint(10000)):
+        lista.append(i)
+    return lista
+
+def cemMil():
+    lista = []
+    for i in range(random.randint(100000)):
+        lista.append(i)
+    return lista
+
+def quiMil():
+    lista = []
+    for i in range(random.randint(500000)):
+        lista.append(i)
+    return lista
+
+def umMilhao():
+    lista = []
+    for i in range(random.randint(1000000)):
+        lista.append(i)
+    return lista
+
+def cincoMilhao():
+    lista = []
+    for i in range(random.randint(10000)):
+        lista.append(i)
+    return lista
+
+
 
 def get_time(arg):
     inicio = time.time()
@@ -6,5 +52,10 @@ def get_time(arg):
     fim = time.time()
     return fim-inicio
 
-dif = get_time(1)
-print(f'Dif: {dif:.2f}')
+def principal():
+    dezMil = dezMil()
+    bs = bubbleSort(dezMil)
+    get_time(bs)
+
+
+principal()
