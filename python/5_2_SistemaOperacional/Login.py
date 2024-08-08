21211085b5b83f7cb1a2a6cf6fb80679b3e3f565
Vinícius Yoda <vyoda4604@gmail.com>
2024-08-08 11:20:24 -0300

    deletando alguns arquivos

--- python/5_2_SistemaOperacional/Login.py
+++
@@ -1,9 +0,0 @@
-import getpass
-
-usuario = input("Digite o usuário: ").upper()
-senha = getpass.getpass("Digite a senha: ")
-
-if usuario == "José" and senha == "1234":
-    print("Usuário logado")
-else:
-    print("Login Negado")
