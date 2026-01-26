9440d129cc8eecbaf5a187b6a9f8e1713fa53b34
Vinícius Yoda <vyoda4604@gmail.com>
2026-01-26 19:23:30 -0300

    u

---
+++ mundo4/rich/rich001.py
@@ -0,0 +1,5 @@
+from rich import print
+
+print("Olá [red]mundo[/]! :earth_americas:")
+print("Este é um texto em [bold magenta]negrito magenta[/] com um emoji :smiley:!")
+print("[underline blue]Texto azul sublinhado[/]")
